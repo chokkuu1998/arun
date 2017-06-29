b831e179c78225382117b64dc042c871a18a8e66
chokkuu1998 <chokkuu1998@gmail.com>
2017-06-29 14:46:37 +0530

    Rename chocku to david.c

---
+++ david.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+clrscr();
+printf("\n enter the number");
+scanf("%d",&a);
+if(a%2==0)
+printf("even");
+else
+printf("odd");
+getch();
+
+}
