f25c83027310d9eb22e1a9b0873eb82124aad0d8
chokkuu1998 <chokkuu1998@gmail.com>
2017-06-29 14:22:15 +0530

    Create chocku.c

---
+++ chocku.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+printf("\n enter the number");
+scanf("%d" &a);
+if(a>0)
+printf("positive");
+else if(a<0)
+printf("negative");
+else
+printf("zero");
+getch();
+}
+
