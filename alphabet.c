2edbce8881962fb4f0ed21d5b3b4feca96dd1964
chokkuu1998 <chokkuu1998@gmail.com>
2017-06-30 16:34:49 +0530

    Create alphabet.c

---
+++ alphabet.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char h;
+clrscr();
+printf("\n enter the alphabet");
+scanf("%c",&h);
+if(c>='a'&&c<='z'||c>='A'&&c<='Z')
+{
+printf("\n alphabet");
+}
+else
+{
+printf("\n not an alphabet");
+}
+getch();
+}
