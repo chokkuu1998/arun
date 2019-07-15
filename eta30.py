eed3ba4bb8d99af0c95082615ebda8c1997c8642
chokkuu1998 <chokkuu1998@gmail.com>
2019-07-15 22:37:21 +0530

    Create eta30.py

---
+++ eta30.py
@@ -0,0 +1,10 @@
+pp,qq=map(int,input().split())
+l=list(map(int,input().split()))
+for i in range(qq):
+    r,s=map(int,input().split())
+    t = l[r-1:s]
+    u = t[0]
+    for i in range(1,len(t)):
+        u = u ^ t[i]
+    print(u)
+    
