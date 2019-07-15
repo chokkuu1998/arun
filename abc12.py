61e1399d5a8a24e7d958d55ee4789c508f15f115
chokkuu1998 <chokkuu1998@gmail.com>
2019-07-16 00:06:42 +0530

    Update abc12.py

--- abc12.py
+++ abc12.py
@@ -1,23 +1,18 @@
-thii,sss=map(int,input().split())
-if thii%10==0:
-  thii=str(thi)
-  c=0
-  for i in range(len(thii)-1,-1,-1):
-    if thii[i]=='0':
-      c+=1
-  if sss<=c:
-    print(thii)
-  else:
-    thii=thii[:-c]
-    thii=thii+'0'*ss
-    print(thii)
-elif 10%(thii%10)==0:
-  no=int('1'+'0'*sss)
-  while True:
-    if no%thi==0:
-      print(no)
-      break
-    else:
-      no+=int('1'+'0'*sss)
-else:
-  print(str(thii)+sss*'0')
+AA=input().split()
+BB=int(AA[1])
+CC=int(AA[0])
+DD=[int(P) for P in AA[0]]
+X=0
+y=1;
+while(X==0):
+	prod=y*C
+	l=[ int(P) for P in str(prod)]
+	k=0
+	j=len(l)-1
+	while(l[j]==0):
+		k=k+1
+		j=j-1
+	if(k>=B):
+		print(prod)
+		X=1
+	y=y+1
