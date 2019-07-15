21fc5a269ff48e1231b07d44b5790c13ed34fd32
chokkuu1998 <chokkuu1998@gmail.com>
2019-07-15 23:59:42 +0530

    Create abc10.py

---
+++ abc10.py
@@ -0,0 +1,13 @@
+nn=(input())
+if (nn==nn[::-1]):
+   print("yes")
+else:
+   a=nn.strip('0')
+   if (a==a[::-1]):
+      print("yes")
+   else:
+      b=n.lstrip('0')
+      if (b==b[::-1]):
+        print("yes")
+      else:
+        print("no")
