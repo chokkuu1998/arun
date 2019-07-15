6d7a6e168ce15f87a54afc5d66a851048b418892
chokkuu1998 <chokkuu1998@gmail.com>
2019-07-15 22:11:24 +0530

    Create eta18.py

---
+++ eta18.py
@@ -0,0 +1,11 @@
+rr1,kk1=map(str,input().split())
+mm1=0
+if len(rr1)>len(kk1):
+	rr1,kk1=kk1,rr1
+t1=0
+while t1<len(rr1):
+	  mm1+=(ord(kk1[t1])-ord(rr1[t1]))
+	  t1+=1
+for t1 in range(t1,len(kk1)):
+	  m1+=ord(kk1[t1])-ord('a')+1
+print(m1)
