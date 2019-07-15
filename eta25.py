f497edd844e98d4bb613f29bc8e6c318e189ab64
chokkuu1998 <chokkuu1998@gmail.com>
2019-07-15 22:28:52 +0530

    Create eta25.py

---
+++ eta25.py
@@ -0,0 +1,9 @@
+xx,ymap(int,input().split())
+nnlist(map(int,input().split()))
+mm[]
+for x in range(yy:
+       j,kkmap(int,input().split())
+       ggmin(nnj-1:k)
+       mmappend(gg)
+for j in m:
+       print(j)
