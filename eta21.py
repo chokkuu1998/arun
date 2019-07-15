d8f69af0347919aeb3d5ca087e1cb445f0e38155
chokkuu1998 <chokkuu1998@gmail.com>
2019-07-15 22:19:44 +0530

    Create eta21.py

---
+++ eta21.py
@@ -0,0 +1,15 @@
+nn=int(input())
+bala=[]
+for i in range(nn):
+  v=input()
+  bal.append(v)
+mval=min(bal,key=len)
+bal.remove(mval)
+for i in range(len(mval)):
+  for j in range(len(bal)):
+     cval=balj]
+     if mval[:i+1]==cval[:i+1]:
+       result=mval[:i+1]
+     else:
+       break
+print(result)
